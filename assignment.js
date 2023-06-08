5c0fa2d81313278b5589fed9394a53a3b09c5ff4
Tesilimi Yusuf <tessyoncom@gmail.com>
2023-06-08 14:44:51 +0100

    updated files

---
+++ assignment.js
@@ -0,0 +1,20 @@
+//Write JS code that solve the following problems
+//Q1. Using an if-else statement, determine if a number is divisible by 3, by 5, or both.
+
+let q1test = 15;
+
+//Add code here
+
+//Q2. Using an if-else statement, determin is a year is a leap year or not.
+//Research what are the conditions to be considered a leap year.
+
+let year = 1999;
+// let year = 2000;
+// let year = 1900;
+// let year = 2024;
+
+//Add code here
+
+//Q3. Use a loop to print all the numbers from 1 to 100 that are divisible by 3 or 4.
+
+//Add code here
